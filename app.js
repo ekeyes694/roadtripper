b205fa47b69a18d48064e90fcb753f7a7107b499
ekeyes694 <ekeyes693@gmail.com>
2016-06-21 18:02:00 -0400

    final

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var app = angular.module('myApp', ['ngAnimate'])
+var app = angular.module('myApp', ['ngAnimate']);
 
-app.controller('helpController', function ($scope) {
+
+
+
+app.controller('helpController', ['$scope', '$http', function ($scope, $http) {
     $scope.show = false;
     $scope.help = function () {
         if ($scope.myButton) {
@@ -9,4 +12,25 @@ app.controller('helpController', function ($scope) {
             $scope.show = false;
         });
     }
-});
+    var weather = new XMLHttpRequest();
+    weather.open("GET", "http://api.wunderground.com/api/fa5715ba5adff2ac/conditions/q/MA/Boston.json", false)
+    weather.send(null);
+
+    var r = JSON.parse(weather.response);
+
+    var dis = "Current location: " + r.current_observation.display_location.full + "  <p>";
+    var temp = r.current_observation.temperature_string + "  <p>";
+    var wind = r.current_observation.wind_string + "  <p>";
+    var icon = r.current_observation.icon + "  <p>";
+
+
+
+    function getWeather(id, res) {
+        document.getElementById(id).innerHTML = res;
+    }
+    getWeather("weather", dis);
+    getWeather("temp", temp);
+    getWeather("wind", wind);
+    getWeather("icon", icon);
+
+            }]);
